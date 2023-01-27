5a61bd52297e554fe9bb2f030fe66e313623b433
francesperez <frances.perez141@gmail.com>
2023-01-27 16:07:29 -0600

    Finally fixed background not being transparent. Stupid thing was fixed with just one line of code. Literally so frustrating but so relieved now

--- src/index.ts
+++ src/index.ts
@@ -28,31 +28,21 @@ import "./styles.css";
 
 async function setupViewer(){
 
-    // Initialize the viewer
     const viewer = new ViewerApp({
         canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
     })
-
-    // Add some plugins
     const manager = await viewer.addPlugin(AssetManagerPlugin)
-
-    // Add a popup(in HTML) with download progress when any asset is downloading.
     await viewer.addPlugin(AssetManagerBasicPopupPlugin)
 
     // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-    // await viewer.addPlugin(GammaCorrectionPlugin)
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
-    // await viewer.addPlugin(DiamondPlugin)
-    // await viewer.addPlugin(FrameFadePlugin)
-    // await viewer.addPlugin(GLTFAnimationPlugin)
-    // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
-    // await viewer.addPlugin(TemporalAAPlugin)
-    // await viewer.addPlugin(AnisotropyPlugin)
+    await viewer.addPlugin(GBufferPlugin)
+    await viewer.addPlugin(new ProgressivePlugin(32))
+    await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
+    await viewer.addPlugin(GammaCorrectionPlugin)
+    await viewer.addPlugin(SSRPlugin)
+    await viewer.addPlugin(SSAOPlugin)
+    await viewer.addPlugin(BloomPlugin)
+
 
     // or use this to add all main ones at once.
     await addBasePlugins(viewer)
@@ -63,7 +53,12 @@ async function setupViewer(){
     // This must be called once after all plugins are added.
     viewer.renderer.refreshPipeline()
 
-    await manager.addFromPath("./assets/bust.glb")
+    await manager.addFromPath("./assets/bustXglossy.glb")
+
+    viewer.getPlugin(TonemapPlugin)!.config!.clipBackground = true;
+
+// Uncomment later: this is in video #2
+    // viewer.scene.activeCamera.setCameraOptions({controlsEnabled: false});
 
     // Load an environment map if not set in the glb file
     // await viewer.scene.setEnvironment(
