1d0ea65556553f4a419c86aca0e77abab5a589f9
francesperez <frances.perez141@gmail.com>
2023-01-28 22:20:29 -0600

    Fixed all three sections with scroll trigger

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,54 @@
 import {
     ViewerApp,
     AssetManagerPlugin,
+    GBufferPlugin,
+    ProgressivePlugin,
     TonemapPlugin,
-    
-    addBasePlugins,
-    TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
-
-    IViewerPlugin,
-
+    SSRPlugin,
+    SSAOPlugin,
+    BloomPlugin,
+    Vector3, GammaCorrectionPlugin, MeshBasicMaterial2, Color
 } from "webgi";
 import "./styles.css";
 
-import gsap from "gsap";
-import {ScrollTrigger} from "gsap/ScrollTrigger";
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 async function setupViewer(){
 
     const viewer = new ViewerApp({
         canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
+        // isAntialiased: true,
     })
+
     const manager = await viewer.addPlugin(AssetManagerPlugin)
-    await viewer.addPlugin(AssetManagerBasicPopupPlugin)
+    const camera = viewer.scene.activeCamera
+    const position = camera.position
+    const target = camera.target
+    const exitButton = document.querySelector('.button--exit') as HTMLElement
+    const customizerInterface = document.querySelector('.customizer--container') as HTMLElement
 
-
-    await addBasePlugins(viewer)
-
-    await viewer.addPlugin(CanvasSnipperPlugin)
+    // Add plugins individually.
+    await viewer.addPlugin(GBufferPlugin)
+    await viewer.addPlugin(new ProgressivePlugin(32))
+    await viewer.addPlugin(new TonemapPlugin(true))
+    await viewer.addPlugin(GammaCorrectionPlugin)
+    await viewer.addPlugin(SSRPlugin)
+    await viewer.addPlugin(SSAOPlugin)
+    await viewer.addPlugin(BloomPlugin)
 
     viewer.renderer.refreshPipeline()
 
-    await manager.addFromPath("./assets/bustXglossy.glb")
+    await manager.addFromPath("./assets/scene2.glb")
 
     viewer.getPlugin(TonemapPlugin)!.config!.clipBackground = true;
 
 // Uncomment later: this is in video 
     // viewer.scene.activeCamera.setCameraOptions({controlsEnabled: false});
 
-    // Load an environment map if not set in the glb file
+    // // Load an environment map if not set in the glb file
     // await viewer.scene.setEnvironment(
     //     await manager.importer!.importSinglePath<ITexture>(
     //         "./assets/environment.hdr"
@@ -46,10 +56,90 @@ async function setupViewer(){
     // );
 
     // Add some UI for tweak and testing.
-    const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
+    // const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
     // Add plugins to the UI to see their settings.
-    uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
+    // uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
+
+
+    function setupScrollAnimation(){
+        const tl = gsap.timeline();
+    // First Section
+        tl
+        .to(position,{
+            x: -1.80, y: 1.75, z: 2.68,
+            scrollTrigger: {
+                trigger: ".section2",
+                start: "top bottom",
+                end: "top top",
+                scrub: true,
+                immediateRender: false
+            },
+            onUpdate
+        })
+
+        .to(".container1", { xPercent:'-150' , opacity:0,
+        scrollTrigger: {
+            trigger: ".section2",
+            start:"top bottom",
+            end: "top 80%", scrub: 1,
+            immediateRender: false
+    }})
+
+        .to(target,{
+            x: 0.27, z: 0.29, y: 0.60,
+            scrollTrigger: {
+                trigger: ".section2",
+                start: "top bottom",
+                end: "top top",
+                scrub: true,
+                immediateRender: false
+            }})
+// Final Section
+        .to(position,{
+            x: -1.42, y: -0.30, z: 2.06,
+            scrollTrigger: {
+                trigger: ".section3",
+                start: "top bottom",
+                end: "top top",
+                scrub: true,
+                immediateRender: false
+            },
+            onUpdate
+        })
+
+        .to(target,{
+            x: -0.70, y: 0.62, z: 0.65,
+            scrollTrigger: {
+                trigger: ".section3",
+                start: "top bottom",
+                end: "top top",
+                scrub: true,
+                immediateRender: false
+            }})
+    
+    }
+
+    setupScrollAnimation();
+
+    let needsUpdate = true;
+
+    function onUpdate(){
+        needsUpdate = true;
+        // viewer.renderer.resetShadows();
+        viewer.setDirty();
+    }
+
+    viewer.addEventListener("preFrame", () => {
+        if (needsUpdate) {
+            camera.positionTargetUpdated(true)
+            needsUpdate = false;
+        }
+    });
 
 }
+
+
+
+
 
 setupViewer()
